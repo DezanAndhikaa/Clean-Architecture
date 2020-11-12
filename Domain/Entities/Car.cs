5c40c5463d2edd4cb2be73a713475ca5efa055a4
Dezan Andhika <dezanestafet@gmail.com>
2020-11-12 13:47:26 +0700

    Init domain

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public Guid IdGuid { get; set; }
+        public string CarName { get; set; }
+        public string CarType { get; set; }
+        public int CarSeat { get; set; }
+        public long CarPrice { get; set; }
+    }
+}
---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Customer
+    {
+        public Guid IdCustomer { get; set; }
+        public string CustomerName { get; set; }
+        public int CustomerGender { get; set; }
+        public string CustomerPhone { get; set; }
+    }
+}
