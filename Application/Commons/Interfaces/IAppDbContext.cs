fc9bfb8ef6d7871b9a299bb4777e091596f6a7db
Dezan Andhika <dezanestafet@gmail.com>
2020-11-12 14:04:54 +0700

    Create IAppDbContext.cs

---
+++ Application/Commons/Interfaces/IAppDbContext.cs
@@ -0,0 +1,13 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Commons.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<Car> Cars { get; set; }
+        DbSet<Customer> Customers { get; set; }
+
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken);
+    }
+}
