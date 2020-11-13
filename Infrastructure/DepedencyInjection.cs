1ce9d0eaabd0eb4e14d817af52a2f9d12b1da68a
Dezan Andhika <dezanestafet@gmail.com>
2020-11-13 09:34:37 +0700

    Add dependency for infra

---
+++ Infrastructure/DepedencyInjection.cs
@@ -0,0 +1,19 @@
+using Infrastructure.Persistences;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DbConnectionString")));
+
+            return services;
+        }
+    }
+}
